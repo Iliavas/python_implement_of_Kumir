7fbad496e362f3fd18a0f6147aa465899fa783c7
ilvas <il.vsl0110@gmail.com>
2020-04-21 23:23:16 +0300

    create color syntax class
    Signed-off-by: ilvas <il.vsl0110@gmail.com>

---
+++ color_syntax.py
@@ -0,0 +1,9 @@
+from utils import start_th
+import tkinter as tk
+#TODO all
+class Coloryfier:
+    words = ['вывод']
+    def __init__(self, cons):
+        self.cons = cons
+    def get_color(self):
+        pass
